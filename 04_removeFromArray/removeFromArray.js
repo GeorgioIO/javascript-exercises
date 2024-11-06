a47fd9289d9f2d312e8b4c4d05e73c20b6d1b906
Georgio <georgiojabbour.g.gj@gmail.com>
2024-11-06 17:57:21 +0200

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(elements , ...args) {
+    
+    return elements.filter(element => !args.includes(element));
 };
 
 // Do not edit below this line
