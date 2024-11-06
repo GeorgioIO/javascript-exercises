3408a88506a345897663d9fe059126755c93b5e1
Georgio <georgiojabbour.g.gj@gmail.com>
2024-11-06 16:28:43 +0200

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(word , reps) {
+    if (reps < 0)
+    {
+        return "ERROR";
+    }
+    
+    let formedString = ""; 
+    
+    for(let i = 0; i < reps; i++)
+    {
+        formedString += word;
+    }
+    return formedString;
 };
 
 // Do not edit below this line
