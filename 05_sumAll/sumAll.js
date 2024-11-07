cc10622f28418a82c6a4e982952226fe6775f481
Georgio <georgiojabbour.g.gj@gmail.com>
2024-11-07 09:22:39 +0200

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(startNum , endNum) {
 
+    if (startNum > endNum)
+    {
+        let num = endNum;
+        endNum = startNum;
+        startNum = num;
+        
+    }
+
+    if (startNum < 0 || endNum <= 0)
+    {
+        return "ERROR";
+    }
+    else if (isNaN(startNum) || isNaN(endNum))
+    {
+        return "ERROR";
+    }
+    else if (!Number.isInteger(startNum) || !Number.isInteger(endNum))
+    {
+        return "ERROR";
+    }
+
+    let sum = 0;
+
+    for (let i = startNum ; i <= endNum; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
