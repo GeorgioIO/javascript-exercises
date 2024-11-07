3fc95623d77335d556db84a697c1eb022ea2b98b
Georgio <georgiojabbour.g.gj@gmail.com>
2024-11-07 10:28:03 +0200

    solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitValue) {
+  return Math.round(5 / 9 * (fahrenheitValue - 32) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusValue) {
+  return Math.round(((9 / 5 * celsiusValue) + 32 )* 10) / 10;
 };
 
 // Do not edit below this line
