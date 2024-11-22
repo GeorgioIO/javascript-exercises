3cd6dc0b40e8cd2ff497388163fc0b161296cf82
Georgio <georgiojabbour.g.gj@gmail.com>
2024-11-22 20:45:04 +0200

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,34 @@
-const add = function() {
-	
+const add = function(a , b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a , b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((som , currentIterator) => {
+    return som + currentIterator;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, currentIterator) => {
+    return total * currentIterator;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a , b) {
+	return a**b
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  let fac = 1
+  for(let i = a ; i >= 1; i--)
+  {
+    fac = i * fac;
+  }
+  return fac
 	
 };
 
