6cfcc934237f150222ab92a9367916b50270424a
Georgio <georgiojabbour.g.gj@gmail.com>
2024-11-22 21:17:50 +0200

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanString = word
+    .toLowerCase()
+    .split("")
+    .filter(w => alphanumerical.includes(w))
+    .join("")
+
+    const reverseString = Array.from(cleanString).reverse().join("")
+
+    return reverseString === cleanString
 };
 
 // Do not edit below this line
