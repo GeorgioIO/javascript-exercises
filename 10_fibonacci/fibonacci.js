aea0e198527f402119528631809304f50470d16c
Georgio <georgiojabbour.g.gj@gmail.com>
2024-11-23 17:08:43 +0200

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(numberToGet) {
+    if (numberToGet == 0)
+    {
+        return 0;
+    }
+    else if(numberToGet < 0)
+    {
+        return "OOPS"
+    }
+    let fiboSequence = [1 , 1]
+    for(let i = 2 ; i <= numberToGet; i++)
+    {
+        fiboSequence = fiboSequence.concat(fiboSequence.slice(i - 2).reduce((total , currentNumber) => {
+            return total + currentNumber
+        }))
+    }
+    return fiboSequence[numberToGet - 1]
 };
 
 // Do not edit below this line
